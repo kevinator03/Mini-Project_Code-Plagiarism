f9e0ff92ef60ff7a35e0d7f942dad64e7ff521f2
kevinator03 <106988538+kevinator03@users.noreply.github.com>
2023-03-16 23:20:37 +0530

    Now replacing function names
    In this commit, I have added the functionality which handles function detection by checking for an opening round bracket somewhere after the occurrence of a data type. After storing the function name by taking a substring, I map it to a dummy name. I am working under the assumption that variable names and function names are mutually exclusive i.e. the name of one is not found as part of the other.

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import nltk
-
-import re
-
-# import nltk for stopwords
-# from nltk.corpus import stopwords
-# stop_words = set(stopwords.words('english'))
-
-program = """public class ReverseNumberExample1   
+# stores the source code
+program = """public class ReverseNumberExample1                 
 {  
 public static void main(String[] args)   // this is the main class
 {  
@@ -18,31 +11,39 @@ reverse = reverse * 10 + remainder;  // taking individual digits of the number b
 number = number/10;  
 }  
 System.out.println("The reverse of the given number is: " + reverse);  // the required output
-}  
+}
+
+public int random(int a)
+{
+System.out.println("Hello there");
+}
 }  """
 
 print("Input Code: ")
 print(program)
 
 flag1 = 0  # set it to 1 when we encounter comment start
-flag2 = [0, 0, 0, 0, 0, 0, 0]  # for variables
+flag2 = 0  # for variables 
+
+data_types = ['int', 'long', 'double',
+              'boolean', 'char', 'string', 'void', 'float']     # to store the data types which will help checking for integers and functions
 
 var_names = []
-var_dict = {}
+func_names = []
 
-var_count = [0, 0, 0, 0, 0, 0, 0]      # to keep track of variables in the following order: int double String char boolean long float
+var_dict = {}           # to store correct mapping of actual variable names to dummy names
+func_dict = {}          # same purpose for functions
+
 ctr = 0                 # to keep count of all variables for dummy variable naming
 
 program = program.lower()
-# program = re.sub(r'\d+','',program)   # removing numbers
-# program = re.sub(r'[^\w\s]','', program)  # removing all punctuation except words and space
+
 program = program.strip()     # remove trailing and leading white spaces
 
-lst_string = [program][0].split('\n')
-# print(lst_string)
-normalised_code = []
+lst_string = [program][0].split('\n')       # breaking the program into list of individual lines
+normalised_code = []        # will eventually store the final normalised code
 
-no_comments_string = ""
+no_comments_string = ""                 # to remove comments
 for line in lst_string:
     line = line.split()
     j = 0
@@ -50,64 +51,54 @@ for line in lst_string:
         if i == '//':
             flag1 = 1
 
-        if i == 'int':
-            flag2[0] = 1
-        if i == 'double':
-            flag2[1] = 1
-        if i == 'string':
-            flag2[2] = 1
-        if i == 'char':
-            flag2[3] = 1
-        if i == 'boolean':
-            flag2[4] = 1
-        if i == 'long':
-            flag2[5] = 1
-        if i == 'float':
-            flag2[6] = 1
-        
-        
-        if i == '=' and flag2[0] == 1:
+        if i in data_types:
+            flag2 = 1
+
+        if i == '=' and flag2 == 1:      # this if statement handles variable detection
             j -= 1
-            # no_comments_string = no_comments_string.replace(no_comments_string[-1], dummy)
             var_names.append(line[j])
             j += 1
-        if i == '=' and flag2[1] == 1:
-            var_count[1] += 1
-        if i == '=' and flag2[2] == 1:
-            var_count[2] += 1
-        if i == '=' and flag2[3] == 1:
-            var_count[3] += 1
-        if i == '=' and flag2[4] == 1:       # lines 69-85 keep track of the various variables
-            var_count[4] += 1
-        if i == '=' and flag2[5] == 1:
-            var_count[5] += 1
-        if i == '=' and flag2[6] == 1:
-            var_count[6] += 1
+
+        if i in data_types:
+            j += 1
+            if '(' in line[j]:          # characteristic of a function is a data type and an opening round bracket soon after
+                loc = line[j].find('(')
+                func_names.append(line[j][0:loc])
+            j -= 1
 
         if flag1 == 0:
             no_comments_string += i + ' '
         j += 1
-    
-    for i in range(0, len(flag2)):
-        flag2[i] = 0
+
+    flag2 = 0
     normalised_code.append(no_comments_string)
     no_comments_string = ""
-    flag1 = 0
+    flag1 = 0   # assuming you don't declare two different types of variable in the same line
 
 for name in var_names:
     dummy = "var" + str(ctr)
-    var_dict[name] = dummy
+    var_dict[name] = dummy      # setting up the mapping of variable names and dummy values
     ctr += 1
 
-for i in range(3, len(normalised_code)):
+ctr = 0     # reset counter variable before starting function mapping
+
+for name in func_names:
+    dummy = "func" + str(ctr)
+    func_dict[name] = dummy
+    ctr += 1
+
+for i in range(3, len(normalised_code)):        # start from line 3 to avoid picking up a variable name as part of the class name which is sometimes common
     for name in var_names:
         if name in normalised_code[i]:
-            normalised_code[i] = normalised_code[i].replace(name, var_dict[name])
+            normalised_code[i] = normalised_code[i].replace(
+                name, var_dict[name])
+
+for i in range(0, len(normalised_code)):
+    for name in func_names:
+        if name in normalised_code[i]:
+            normalised_code[i] = normalised_code[i].replace(
+                name, func_dict[name])
 
 print("\nNormalised code: ")
 for line in normalised_code:
     print(line)
-
-
-# print("Number of variables are: ")
-# print(f"Integers: {var_count[0]}, Double: {var_count[1]}, Strings: {var_count[2]}, Char: {var_count[3]}, Boolean: {var_count[4]}, Long: {var_count[5]}, Float: {var_count[6]}")
