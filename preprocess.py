5f7cc0bb7c28456a664ef20040a6fde28ac53258
Kevin Suvarna <106988538+kevinsuvarna@users.noreply.github.com>
2023-05-11 15:32:01 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,63 @@
 import pyparsing
 import re
+import io
+import  tokenize
+
+def remove_comments_and_docstrings(source):
+    """
+    Returns 'source' minus comments and docstrings.
+    """
+    io_obj = io.StringIO(source)
+    out = ""
+    prev_toktype = tokenize.INDENT
+    last_lineno = -1
+    last_col = 0
+    for tok in tokenize.generate_tokens(io_obj.readline):
+        token_type = tok[0]
+        token_string = tok[1]
+        start_line, start_col = tok[2]
+        end_line, end_col = tok[3]
+        ltext = tok[4]
+        # The following two conditionals preserve indentation.
+        # This is necessary because we're not using tokenize.untokenize()
+        # (because it spits out code with copious amounts of oddly-placed
+        # whitespace).
+        if start_line > last_lineno:
+            last_col = 0
+        if start_col > last_col:
+            out += (" " * (start_col - last_col))
+        # Remove comments:
+        if token_type == tokenize.COMMENT:
+            pass
+        # This series of conditionals removes docstrings:
+        elif token_type == tokenize.STRING:
+            if prev_toktype != tokenize.INDENT:
+        # This is likely a docstring; double-check we're not inside an operator:
+                if prev_toktype != tokenize.NEWLINE:
+                    # Note regarding NEWLINE vs NL: The tokenize module
+                    # differentiates between newlines that start a new statement
+                    # and newlines inside of operators such as parens, brackes,
+                    # and curly braces.  Newlines inside of operators are
+                    # NEWLINE and newlines that start new code are NL.
+                    # Catch whole-module docstrings:
+                    if start_col > 0:
+                        # Unlabelled indentation means we're inside an operator
+                        out += token_string
+                    # Note regarding the INDENT token: The tokenize module does
+                    # not label indentation inside of an operator (parens,
+                    # brackets, and curly braces) as actual indentation.
+                    # For example:
+                    # def foo():
+                    #     "The spaces before this docstring are tokenize.INDENT"
+                    #     test = [
+                    #         "The spaces before this string do not get a token"
+                    #     ]
+        else:
+            out += token_string
+        prev_toktype = token_type
+        last_col = end_col
+        last_lineno = end_line
+    return out
 
 def preprocessing(prog):
     flag1 = 0  # to check if a data type is encountered 
@@ -16,6 +74,8 @@ def preprocessing(prog):
     comment = pyparsing.nestedExpr("/*","*/").suppress() # using a parser to remove multi-line comments quickly
     prog = comment.transformString(prog)
 
+    prog = remove_comments_and_docstrings(prog)
+
     prog = prog.strip()     # remove trailing and leading white spaces
 
     lst_string = [prog][0].split('\n')       # breaking the prog into list of individual lines
@@ -27,6 +87,21 @@ def preprocessing(prog):
         line = line.split()
         for i in line:
             loc = i.find('//')
+            if loc >= 0:
+                no_comments_string += i[0:loc] + ' '
+                break
+            no_comments_string += i + ' '
+        if no_comments_string != '':
+            no_comments_code.append(no_comments_string)
+        no_comments_string = ""
+
+    lst_string = no_comments_code
+    no_comments_code = []
+
+    for line in lst_string:
+        line = line.split()
+        for i in line:
+            loc = i.find('#')
             if loc >= 0:
                 no_comments_string += i[0:loc] + ' '
                 break
